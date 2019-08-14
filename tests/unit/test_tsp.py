e0f7a7296c6f2d4a91ea06225d838f715f4aa8ba
Bazyli Cyran <bazyli.cyran@gmail.com>
2019-08-14 16:14:28 +0200

    Improve tests formatting

--- tests/unit/test_tsp.py
+++ tests/unit/test_tsp.py
@@ -14,14 +14,16 @@ class TestPath(unittest.TestCase):
             self.assertEqual(result.distance, -1)
 
     def test_set_stop(self):
-        data = [(1, 5, [-1, 5, -1, -1, -1, -1]),
-                (0, 0, [0, 5, -1, -1, -1, -1]),
-                (5, 9, [0, 5, -1, -1, -1, 9]),
-                (2, 3, [0, 5, 3, -1, -1, 9]),
-                (3, 7, [0, 5, 3, 7, -1, 9]),
-                (4, 6, [0, 5, 3, 7, 6, 9]),
-                (0, 4, [4, 5, 3, 7, 6, 9]),
-                (-1, 8, [4, 5, 3, 7, 6, 8])]
+        data = [
+            (1, 5, [-1, 5, -1, -1, -1, -1]),
+            (0, 0, [0, 5, -1, -1, -1, -1]),
+            (5, 9, [0, 5, -1, -1, -1, 9]),
+            (2, 3, [0, 5, 3, -1, -1, 9]),
+            (3, 7, [0, 5, 3, 7, -1, 9]),
+            (4, 6, [0, 5, 3, 7, 6, 9]),
+            (0, 4, [4, 5, 3, 7, 6, 9]),
+            (-1, 8, [4, 5, 3, 7, 6, 8])
+        ]
 
         path = Path(6)
 
@@ -60,9 +62,11 @@ class TestPath(unittest.TestCase):
                 self.assertRaises(IndexError, path.get_stop, index)
 
     def test_set_path(self):
-        data = [[1, 2, 3, 4, 5, 6],
-                [9, 20, 1, 5, 55, 7],
-                [0, 0, 0, 0, 0, 0]]
+        data = [
+            [1, 2, 3, 4, 5, 6],
+            [9, 20, 1, 5, 55, 7],
+            [0, 0, 0, 0, 0, 0]
+        ]
 
         path = Path(6)
 
@@ -72,7 +76,12 @@ class TestPath(unittest.TestCase):
                 self.assertListEqual(path._path, p)
 
     def test_set_path_exception(self):
-        data = [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5, 6, 7], [1], []]
+        data = [
+            [1, 2, 3, 4, 5],
+            [1, 2, 3, 4, 5, 6, 7],
+            [1],
+            []
+        ]
 
         path = Path(6)
 
@@ -81,9 +90,11 @@ class TestPath(unittest.TestCase):
                 self.assertRaises(ValueError, path.set_path, p)
 
     def test_get_path(self):
-        data = [[1, 2, 3, 4, 5, 6],
-                [9, 20, 1, 5, 55, 7],
-                [0, 0, 0, 0, 0, 0]]
+        data = [
+            [1, 2, 3, 4, 5, 6],
+            [9, 20, 1, 5, 55, 7],
+            [0, 0, 0, 0, 0, 0]
+        ]
 
         path = Path(6)
 
@@ -94,16 +105,18 @@ class TestPath(unittest.TestCase):
                 self.assertListEqual(result, p)
 
     def test_in_path(self):
-        data = [(4, None, True),
-                (2, None, True),
-                (7, None, True),
-                (3, None, False),
-                (7, 6, True),
-                (4, 1, True),
-                (8, 3, True),
-                (4, 0, False),
-                (7, 5, False),
-                (10, 3, False)]
+        data = [
+            (4, None, True),
+            (2, None, True),
+            (7, None, True),
+            (3, None, False),
+            (7, 6, True),
+            (4, 1, True),
+            (8, 3, True),
+            (4, 0, False),
+            (7, 5, False),
+            (10, 3, False)
+        ]
 
         path = Path(6)
         path.set_path([4, 1, 8, 2, 9, 7])
@@ -114,12 +127,14 @@ class TestPath(unittest.TestCase):
                 self.assertEqual(result, expected)
 
     def test_swap(self):
-        data = [(1, 3, [1, 4, 3, 2, 5, 6]),
-                (0, 4, [5, 4, 3, 2, 1, 6]),
-                (2, 5, [5, 4, 6, 2, 1, 3]),
-                (5, 0, [3, 4, 6, 2, 1, 5]),
-                (2, 3, [3, 4, 2, 6, 1, 5]),
-                (4, 4, [3, 4, 2, 6, 1, 5])]
+        data = [
+            (1, 3, [1, 4, 3, 2, 5, 6]),
+            (0, 4, [5, 4, 3, 2, 1, 6]),
+            (2, 5, [5, 4, 6, 2, 1, 3]),
+            (5, 0, [3, 4, 6, 2, 1, 5]),
+            (2, 3, [3, 4, 2, 6, 1, 5]),
+            (4, 4, [3, 4, 2, 6, 1, 5])
+        ]
 
         path = Path(6)
         path.set_path([1, 2, 3, 4, 5, 6])
@@ -131,12 +146,14 @@ class TestPath(unittest.TestCase):
                 self.assertListEqual(result, expected)
 
     def test_insert(self):
-        data = [(1, 3, [1, 3, 4, 2, 5, 6]),
-                (0, 4, [3, 4, 2, 5, 1, 6]),
-                (2, 5, [3, 4, 5, 1, 6, 2]),
-                (5, 0, [2, 3, 4, 5, 1, 6]),
-                (2, 3, [2, 3, 5, 4, 1, 6]),
-                (4, 4, [2, 3, 5, 4, 1, 6])]
+        data = [
+            (1, 3, [1, 3, 4, 2, 5, 6]),
+            (0, 4, [3, 4, 2, 5, 1, 6]),
+            (2, 5, [3, 4, 5, 1, 6, 2]),
+            (5, 0, [2, 3, 4, 5, 1, 6]),
+            (2, 3, [2, 3, 5, 4, 1, 6]),
+            (4, 4, [2, 3, 5, 4, 1, 6])
+        ]
 
         path = Path(6)
         path.set_path([1, 2, 3, 4, 5, 6])
@@ -148,12 +165,14 @@ class TestPath(unittest.TestCase):
                 self.assertListEqual(result, expected)
 
     def test_invert(self):
-        data = [(1, 3, [1, 4, 3, 2, 5, 6]),
-                (0, 4, [5, 2, 3, 4, 1, 6]),
-                (2, 5, [5, 2, 6, 1, 4, 3]),
-                (5, 0, [3, 4, 1, 6, 2, 5]),
-                (2, 3, [3, 4, 6, 1, 2, 5]),
-                (4, 4, [3, 4, 6, 1, 2, 5])]
+        data = [
+            (1, 3, [1, 4, 3, 2, 5, 6]),
+            (0, 4, [5, 2, 3, 4, 1, 6]),
+            (2, 5, [5, 2, 6, 1, 4, 3]),
+            (5, 0, [3, 4, 1, 6, 2, 5]),
+            (2, 3, [3, 4, 6, 1, 2, 5]),
+            (4, 4, [3, 4, 6, 1, 2, 5])
+        ]
 
         path = Path(6)
         path.set_path([1, 2, 3, 4, 5, 6])
@@ -165,9 +184,11 @@ class TestPath(unittest.TestCase):
                 self.assertListEqual(result, expected)
 
     def test_move(self):
-        data = [(Path.Neighbourhood.SWAP, 1, 4, [1, 5, 3, 4, 2, 6]),
-                (Path.Neighbourhood.INSERT, 1, 4, [1, 3, 4, 2, 5, 6]),
-                (Path.Neighbourhood.INVERT, 1, 4, [1, 5, 2, 4, 3, 6])]
+        data = [
+            (Path.Neighbourhood.SWAP, 1, 4, [1, 5, 3, 4, 2, 6]),
+            (Path.Neighbourhood.INSERT, 1, 4, [1, 3, 4, 2, 5, 6]),
+            (Path.Neighbourhood.INVERT, 1, 4, [1, 5, 2, 4, 3, 6])
+        ]
 
         path = Path(6)
         path.set_path([1, 2, 3, 4, 5, 6])
@@ -184,10 +205,12 @@ class TestTSP(unittest.TestCase):
     def setUp(self):
         self.tsp = TSP()
         self.tsp._tsplib = TSPLib()
-        self.distances = [[0,   2,  3, 4],
-                          [5,   0,  7, 8],
-                          [9,  10,  0, 12],
-                          [13, 14, 15, 0]]
+        self.distances = [
+            [0,   2,  3, 4],
+            [5,   0,  7, 8],
+            [9,  10,  0, 12],
+            [13, 14, 15, 0]
+        ]
 
     @patch('tspvisual.tsp.TSPLib.weight')
     def test_calc_distances(self, mock_tsplib):
@@ -199,10 +222,12 @@ class TestTSP(unittest.TestCase):
         self.assertListEqual(self.tsp.distances, self.distances)
 
     def test_dist(self):
-        data = [(1, 3, 8),
-                (3, 3, 0),
-                (3, 2, 15),
-                (1, 2, 7)]
+        data = [
+            (1, 3, 8),
+            (3, 3, 0),
+            (3, 2, 15),
+            (1, 2, 7)
+        ]
 
         self.tsp.distances = self.distances
 
@@ -212,11 +237,13 @@ class TestTSP(unittest.TestCase):
                 self.assertEqual(result, expected)
 
     def test_path_dist(self):
-        data = [([0, 1, 2, 3], 2 + 7 + 12),
-                ([2, 0, 1, 3], 9 + 2 + 8),
-                ([0, 3, 1, 0], 4 + 14 + 5),
-                ([1, 1, 1, 1], 0),
-                ([-1, -1, -1, -1], 0)]
+        data = [
+            ([0, 1, 2, 3], 2 + 7 + 12),
+            ([2, 0, 1, 3], 9 + 2 + 8),
+            ([0, 3, 1, 0], 4 + 14 + 5),
+            ([1, 1, 1, 1], 0),
+            ([-1, -1, -1, -1], 0)
+        ]
 
         self.tsp.distances = self.distances
 
--- tests/unit/test_tsplib.py
+++ tests/unit/test_tsplib.py
@@ -75,8 +75,14 @@ class TestTSPLib(unittest.TestCase):
                 self.assertEqual(self.tsplib.specification[key], expected)
 
     def test_parse_coords(self):
-        data = ['1 1 1', '2	2	5', '    3    4    9', '4 5.3 7',
-                '5 9.7 10.1', 'EOF']
+        data = [
+            '1 1 1',
+            '2	2	5',
+            '    3    4    9',
+            '4 5.3 7',
+            '5 9.7 10.1',
+            'EOF'
+        ]
         expected = [(1, 1), (2, 5), (4, 9), (5.3, 7), (9.7, 10.1)]
 
         self.tsplib._lines = Lines(data)
@@ -86,11 +92,18 @@ class TestTSPLib(unittest.TestCase):
         self.assertEqual(self.tsplib._lines.current, data[-1])
 
     def test_parse_weights(self):
-        data = ['0 2 3 4', ' 5    0 7 8', '9 10 0 12 13 14 15 0', 'EOF']
-        expected = [[0,  2,  3,  4],
-                    [5,  0,  7,  8],
-                    [9,  10, 0, 12],
-                    [13, 14, 15, 0]]
+        data = [
+            '0 2 3 4',
+            ' 5    0 7 8',
+            '9 10 0 12 13 14 15 0',
+            'EOF'
+        ]
+        expected = [
+            [0,  2,  3,  4],
+            [5,  0,  7,  8],
+            [9,  10, 0, 12],
+            [13, 14, 15, 0]
+        ]
 
         self.tsplib.specification = {
             'DIMENSION': len(expected),
@@ -103,9 +116,18 @@ class TestTSPLib(unittest.TestCase):
         self.assertEqual(self.tsplib._lines.current, data[-1])
 
     def test_parse(self):
-        data = ['DIMENSION : 3', 'EDGE_WEIGHT_FORMAT : FULL_MATRIX',
-                'EDGE_WEIGHT_SECTION', '1 2 3 4 5', '6 7 8 9',
-                'NODE_COORD_SECTION', '1 1 1', '2 4 5', '3 4.20 69.9', 'EOF']
+        data = [
+            'DIMENSION : 3',
+            'EDGE_WEIGHT_FORMAT : FULL_MATRIX',
+            'EDGE_WEIGHT_SECTION',
+            '1 2 3 4 5',
+            '6 7 8 9',
+            'NODE_COORD_SECTION',
+            '1 1 1',
+            '2 4 5',
+            '3 4.20 69.9',
+            'EOF'
+        ]
         expected_spec = {'DIMENSION': 3, 'EDGE_WEIGHT_FORMAT': 'FULL_MATRIX'}
         expected_weigths = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
         expected_coords = [(1, 1), (4, 5), (4.2, 69.9)]
@@ -119,7 +141,13 @@ class TestTSPLib(unittest.TestCase):
         self.assertListEqual(self.tsplib.coords, expected_coords)
 
     def test_nint(self):
-        data = [(1.0, 1), (2.1, 2), (3.4, 3), (4.5, 5), (5.6, 6)]
+        data = [
+            (1.0, 1),
+            (2.1, 2),
+            (3.4, 3),
+            (4.5, 5),
+            (5.6, 6)
+        ]
 
         for num, expected in data:
             with self.subTest(num=num):
@@ -128,9 +156,21 @@ class TestTSPLib(unittest.TestCase):
                 self.assertIsInstance(result, int)
 
     def test_w_euc_2d(self):
-        coords = [(1, 1), (5, 5), (8, 15), (-50, 10), (-1000, -1000)]
-        data = [(0, 1, 6), (1, 0, 6), (2, 1, 10), (2, 3, 58),
-                (0, 4, 1416), (3, 4, 1387)]
+        coords = [
+            (1, 1),
+            (5, 5),
+            (8, 15),
+            (-50, 10),
+            (-1000, -1000)
+        ]
+        data = [
+            (0, 1, 6),
+            (1, 0, 6),
+            (2, 1, 10),
+            (2, 3, 58),
+            (0, 4, 1416),
+            (3, 4, 1387)
+        ]
 
         self.tsplib.coords = coords
 
@@ -141,10 +181,16 @@ class TestTSPLib(unittest.TestCase):
                 self.assertIsInstance(result, int)
 
     def test_weight(self):
-        weights = [[0,  2,  3],
-                   [4,  0,  6],
-                   [-1, 8, 0]]
-        coords = [(0, 1, 2), (1, 2, 1), (2, -2, 0)]
+        weights = [
+            [0,  2,  3],
+            [4,  0,  6],
+            [-1, 8, 0]
+        ]
+        coords = [
+            (0, 1, 2),
+            (1, 2, 1),
+            (2, -2, 0)
+        ]
         data = [
             ('EXPLICIT', 2, 1, 8),
             ('EXPLICIT', 2, 0, 3),
