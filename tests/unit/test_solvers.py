5ad3b28b57586558585f38feea7ef3ac93794e51
Bazyli Cyran <bazyli.cyran@gmail.com>
2019-09-01 22:34:21 +0200

    Refactor GA unit tests, add NWOX tests

--- tests/unit/test_solvers.py
+++ tests/unit/test_solvers.py
@@ -22,43 +22,41 @@ class TestGASolver(TestCase):
              [0, 4, 1, 2, 3, 5, 0], (3, 5))
         ]
 
-    @patch('tspvisual.solvers.ga.GASolver._rand_subpath')
-    @patch('tspvisual.tsp.TSP.path_dist')
-    def test_crossover_ox(self, mock_dist, mock_rand_subpath):
+    def test_crossover_ox(self):
         expected = [
             [0, 4, 7, 3, 6, 2, 5, 1, 8, 9],
             [6, 1, 2, 3, 0, 4, 5],
             [0, 4, 2, 3, 5, 1, 0]
         ]
 
-        # No instance loaded so we cant calculate distance
-        mock_dist.return_value = 0
+        self._test_crossover(self.gasolver._crossover_ox, expected)
 
-        for (p1, p2, subpath), exp in zip(self.data, expected):
-            with self.subTest(p1=p1, p2=p2, subpath=subpath):
-                mock_rand_subpath.return_value = subpath
-                self.gasolver.tsp.dimension = len(p1) - 1
-                parent1 = Path(path=p1)
-                parent2 = Path(path=p2)
-                child = self.gasolver._crossover_ox(parent1, parent2)
-                self.assertListEqual(child.get_path(), exp)
-
-    @patch('tspvisual.solvers.ga.GASolver._rand_subpath')
-    @patch('tspvisual.tsp.TSP.path_dist')
-    def test_crossover_pmx(self, mock_dist, mock_rand_subpath):
+    def test_crossover_pmx(self):
         expected = [
             [0, 7, 4, 3, 6, 2, 5, 1, 8, 9],
             [0, 1, 2, 3, 6, 4, 5],
             [0, 4, 2, 3, 5, 1, 0]
         ]
 
-        mock_dist.return_value = 0
+        self._test_crossover(self.gasolver._crossover_pmx, expected)
 
+    def test_crossover_nwox(self):
+        expected = [
+            [0, 4, 7, 3, 6, 2, 5, 1, 8, 9],
+            [6, 1, 2, 3, 0, 4, 5],
+            [0, 4, 2, 3, 5, 1, 0]
+        ]
+
+        self._test_crossover(self.gasolver._crossover_nwox, expected)
+
+    @patch('tspvisual.tsp.TSP.path_dist', return_value=0)
+    @patch('tspvisual.solvers.ga.GASolver._rand_subpath')
+    def _test_crossover(self, fun, expected, mock_rand_subpath, mock_dist):
         for (p1, p2, subpath), exp in zip(self.data, expected):
             with self.subTest(p1=p1, p2=p2, subpath=subpath):
                 mock_rand_subpath.return_value = subpath
                 self.gasolver.tsp.dimension = len(p1) - 1
                 parent1 = Path(path=p1)
                 parent2 = Path(path=p2)
-                child = self.gasolver._crossover_pmx(parent1, parent2)
+                child = fun(parent1, parent2)
                 self.assertListEqual(child.get_path(), exp)
