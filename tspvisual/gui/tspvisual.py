47f3f9d9ca39c98f287985ead43cef5af13abb8e
Bazyli Cyran <bazyli.cyran@gmail.com>
2019-09-13 17:39:08 +0200

    Add 'about' dialog

--- tspvisual/gui/tspvisual.py
+++ tspvisual/gui/tspvisual.py
@@ -1,4 +1,5 @@
 import wx
+import wx.adv
 import wx.lib.inspection
 
 from tspvisual.gui.helpers import borders
@@ -36,9 +37,9 @@ class TSPVisual(wx.Frame):
         open_mi = file_menu.Append(wx.ID_OPEN, 'Open', 'Open instance.')
         close_mi = file_menu.Append(wx.ID_CLOSE, 'Close', 'Close instance.')
         file_menu.AppendSeparator()
-        inspect_mi = file_menu.Append(wx.ID_ANY, 'Debug', 'Debug GUI.')
         exit_mi = file_menu.Append(wx.ID_EXIT, 'Exit', 'Exit application.')
         help_menu = wx.Menu()
+        debug_mi = help_menu.Append(wx.ID_ANY, 'Debug', 'Debug GUI.')
         about_mi = help_menu.Append(wx.ID_ANY, 'About', 'About this program.')
         menu_bar.Append(file_menu, 'File')
         menu_bar.Append(help_menu, 'Help')
@@ -73,7 +74,7 @@ class TSPVisual(wx.Frame):
         self.Bind(wx.EVT_MENU, self._on_open, open_mi)
         self.Bind(wx.EVT_MENU, self._on_close, close_mi)
         self.Bind(wx.EVT_MENU, wx.lib.inspection.InspectionTool().Show,
-                  inspect_mi)
+                  debug_mi)
         self.Bind(wx.EVT_MENU, lambda e: self.Close(), exit_mi)
         self.Bind(wx.EVT_MENU, self._on_about, about_mi)
 
@@ -119,7 +120,22 @@ class TSPVisual(wx.Frame):
         self.tsp_info.reset()
 
     def _on_about(self, event):
-        """Handles 'about' menu item.
+        """Shows about program box when.
         """
 
-        pass
+        desccription = 'TSP Visual is a program visualising TSP instances ' \
+                       'and process of solving them for educational purposes.'
+
+        license = 'No license for now.'
+
+        about = wx.adv.AboutDialogInfo()
+        about.SetName('TSP Visual')
+        about.SetVersion('0.1')
+        about.SetDescription(desccription)
+        about.SetCopyright('(C) 2019 Bazyli Cyran')
+        about.SetWebSite('https://bazylicyran.pl/')
+        about.SetLicense(license)
+        about.AddDeveloper('Bazyli Cyran')
+        about.AddDocWriter('Bazyli Cyran')
+
+        wx.adv.AboutBox(about)
