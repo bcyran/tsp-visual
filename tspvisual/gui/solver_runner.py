387f8af6e75d4ec68a55aaae60fee14c580ba901
Bazyli Cyran <bazyli.cyran@gmail.com>
2019-10-25 19:49:53 +0200

    Use multiprocessing to run the solvers

--- tspvisual/gui/solver_runner.py
+++ tspvisual/gui/solver_runner.py
@@ -1,13 +1,15 @@
+import multiprocessing
+import threading
 import time
-from threading import Event, Thread
 
 import wx
 from pubsub import pub
 
 
-class SolverRunner(Thread):
-    """Separate thread for running solver without interfering with other tasks.
-    This thread regularly sends pubsub messages with new solver state.
+class SolverRunner(threading.Thread):
+    """Separate thread for running solver. It spawns SolverProcess, reads
+    solver states from the queue respecting set delay and send pubsub messages
+    to the GUI with new states.
     """
 
     def __init__(self, solver, tsp):
@@ -17,48 +19,83 @@ class SolverRunner(Thread):
         :param TSP tsp: TSP instance.
         """
 
-        Thread.__init__(self)
+        threading.Thread.__init__(self)
+
+        # Results queue
+        self.results = multiprocessing.SimpleQueue()
 
         # Solver and TSP instance
         self._solver = solver
         self._tsp = tsp
 
-        # Event signalling thread to stop
-        self._stop_event = Event()
+        # Event signalling this thread to stop
+        self._stop_event = threading.Event()
 
-        # Delay between solver iterations
+        # Delay between messages to GUI
         self.delay = 0
-        # Limit of solver state messages per second
-        self.message_limit = 60
 
     def run(self):
-        """Runs the solver in step mode and publishes new states.
+        """Creates a process wich runs the solver.
         """
 
-        # Time after next message can be sent
-        next_message_time = time.time()
+        # Start solver process
+        self.solver_process = SolverProcess(
+            self._solver, self._tsp, self.results)
+        self.solver_process.start()
 
-        # Run the solver
-        state = None
-        for state in self._solver.solve(self._tsp):
-            # Stop the solver is stop event is set
-            if self._stop_event.is_set():
-                return
+        # Start reading SolverState objects from the queue
+        while not self._stop_event.is_set():
+            state = self.results.get()
 
-            # Send message if it's the time
-            if time.time() > next_message_time:
-                wx.CallAfter(pub.sendMessage, 'SOLVER_STATE_CHANGE',
-                             state=state)
-                next_message_time = time.time() + 1 / self.message_limit
+            # Send message with the new state to GUI
+            wx.CallAfter(pub.sendMessage, 'SOLVER_STATE_CHANGE', state=state)
 
-            # Sleep for spcified time
+            # Break out of the loop if it's the final state
+            if state.final:
+                break
+
+            # Sleep specified amount of itme
             time.sleep(self.delay)
 
-        # Always send message about the final state
-        wx.CallAfter(pub.sendMessage, 'SOLVER_STATE_CHANGE', state=state)
+        # Wait for the solver process
+        self.solver_process.join()
 
     def stop(self):
-        """Sets the stop event in the thread causing it to end.
+        """Terminates the solver process.
+        """
+
+        self._stop_event.set()
+        self.solver_process.stop()
+
+
+class SolverProcess(multiprocessing.Process):
+    """Separate process for running solver so it can have as much CPU power as
+    it wants without interfering with GUI.
+    """
+
+    def __init__(self, solver, tsp, queue):
+        multiprocessing.Process.__init__(self)
+
+        # Queue for storing results
+        self._solver = solver
+        self._tsp = tsp
+        self._queue = queue
+
+        # Signal to stop the process
+        self._stop_event = multiprocessing.Event()
+
+    def run(self):
+        """Runs the solver in loop, stops on event.
+        """
+
+        for state in self._solver.solve(self._tsp):
+            self._queue.put(state)
+
+            if self._stop_event.is_set():
+                break
+
+    def stop(self):
+        """Sets the stop event.
         """
 
         self._stop_event.set()
