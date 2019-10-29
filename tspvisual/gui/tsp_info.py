c5dd3320e5e71148c1eeed085284bb47e9ba5600
Bazyli Cyran <bazyli.cyran@gmail.com>
2019-10-29 13:55:07 +0100

    Display optimal tour info in info tab

--- tspvisual/gui/tsp_info.py
+++ tspvisual/gui/tsp_info.py
@@ -1,6 +1,8 @@
 import wx
 from pubsub import pub
 
+from tspvisual.gui.helpers import borders
+
 
 class TSPInfo(wx.Panel):
     """Displays specification of the TSP instance.
@@ -14,13 +16,29 @@ class TSPInfo(wx.Panel):
         """Builds GUI.
         """
 
-        sizer = wx.BoxSizer(wx.HORIZONTAL)
+        sizer = wx.BoxSizer(wx.VERTICAL)
 
-        # Specification table
-        self.table = wx.ListCtrl(self, style=wx.LC_REPORT)
-        self.table.InsertColumn(0, 'Key')
-        self.table.InsertColumn(1, 'Value')
-        sizer.Add(self.table, 1, wx.EXPAND | wx.ALL, 10)
+        # TSP specification label
+        tsp_label = wx.StaticText(self, label='TSP instance')
+        sizer.Add(tsp_label, 0, wx.EXPAND | borders('tlr'), 10)
+        # TSP specification table
+        self.tsp_table = wx.ListCtrl(self, style=wx.LC_REPORT)
+        self.tsp_table.InsertColumn(0, 'Key')
+        self.tsp_table.InsertColumn(1, 'Value')
+        sizer.Add(self.tsp_table, 1, wx.EXPAND | borders('lr'), 10)
+
+        # Separator line
+        separator = wx.StaticLine(self)
+        sizer.Add(separator, 0, wx.EXPAND | borders('trbl'), 10)
+
+        # Tour specification label
+        tour_label = wx.StaticText(self, label='Optimal tour')
+        sizer.Add(tour_label, 0, wx.EXPAND | borders('lr'), 10)
+        # Tour specification table
+        self.tour_table = wx.ListCtrl(self, style=wx.LC_REPORT)
+        self.tour_table.InsertColumn(0, 'Key')
+        self.tour_table.InsertColumn(1, 'Key')
+        sizer.Add(self.tour_table, 1, wx.EXPAND | borders('lr'), 10)
 
         self.SetSizer(sizer)
 
@@ -36,14 +54,31 @@ class TSPInfo(wx.Panel):
             return
 
         for key, value in tsp.specification.items():
-            self.table.Append((key, value))
+            self.tsp_table.Append((key, value))
 
-        self.table.SetColumnWidth(0, wx.LIST_AUTOSIZE)
-        self.table.SetColumnWidth(1, wx.LIST_AUTOSIZE)
+        if tsp.opt_tour:
+            for key, value in tsp.opt_tour.specification.items():
+                self.tour_table.Append((key, value))
+
+            self.tour_table.Append(('TOUR', str(tsp.opt_tour.tour)))
+            self.tour_table.Append(('DISTANCE', str(tsp.opt_path.distance)))
+
+        # Not pretty but forces first columns of both tables to be of equal
+        # width
+        self.tsp_table.SetColumnWidth(0, wx.LIST_AUTOSIZE)
+        self.tour_table.SetColumnWidth(0, wx.LIST_AUTOSIZE)
+        width1 = self.tsp_table.GetColumnWidth(0)
+        width2 = self.tour_table.GetColumnWidth(0)
+        max_width = max([width1, width2])
+        self.tsp_table.SetColumnWidth(0, max_width)
+        self.tour_table.SetColumnWidth(0, max_width)
+        self.tsp_table.SetColumnWidth(1, wx.LIST_AUTOSIZE)
+        self.tour_table.SetColumnWidth(1, wx.LIST_AUTOSIZE)
 
     def reset(self):
         """Resets this panel to empty state.
         """
 
         self.specification = {}
-        self.table.DeleteAllItems()
+        self.tsp_table.DeleteAllItems()
+        self.tour_table.DeleteAllItems()
